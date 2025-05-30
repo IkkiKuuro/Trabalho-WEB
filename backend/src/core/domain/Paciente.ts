8870f41479af556fc0fd0614ac764596ce4a33e8
IkkiKuuro <iranildocarlosd@gmail.com>
2025-05-30 14:14:48 -0300

    Mexendo na arquitetura

--- backend/src/core/domain/Paciente.ts
+++ backend/src/core/domain/Paciente.ts
@@ -2,12 +2,12 @@ import { UserRole } from "../../shared/types/common";
 import { User } from "./User";
 
 export interface Paciente extends User {
-  id: string;
-  nome: string;
-  email: string;
-  passwordHash: string; // Armazenar o hash da senha
   tipo: UserRole.PACIENTE;
   data_nascimento: Date;
-  cpf: string; // Deve ser validado para garantir que é único
-  contato_emergencia?: string; // Opcional, pode ser usado para armazenar o contato de emergência do paciente
+  cpf: string; 
+  contato_emergencia?: string; 
+}
+
+export interface PacienteHashDeSenha extends Paciente {
+    hash_de_senha: string;
 }
