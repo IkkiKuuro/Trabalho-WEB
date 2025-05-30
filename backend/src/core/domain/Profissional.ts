b883b622f508afde6810fec7bddead395680b8bd
IkkiKuuro <iranildocarlosd@gmail.com>
2025-05-30 15:57:09 -0300

    Profissional Resolvido

--- backend/src/core/domain/Profissional.ts
+++ backend/src/core/domain/Profissional.ts
@@ -1,6 +1,8 @@
-import { User } from './User';
+import { User } from "./User";
+import { UserRole } from "../../shared/types/common";
 
 export interface Profissional extends User {
+  tipo: UserRole.PROFISSIONAL;
   id: string;
   crm: string;// Deve ser validado para garantir que é único
   especialidade: string;
